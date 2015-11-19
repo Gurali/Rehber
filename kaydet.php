ce5462bd195eb609917bf1f8ae6578d75de1954a
Gurali <rutz.zari@gmail.com>
2015-11-19 16:41:09 +0200

    Update and rename LICENSE.md to kaydet.php

---
+++ kaydet.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<FORM action="kaydett.php" method="post">
+Adı:<INPUT TYPE="TEXT" name="ad" value="">
+<br><br>
+Soyadı:<INPUT TYPE="TEXT" name="soyad" value="">
+<br><br>
+Telefonu:<INPUT TYPE="TEXT" name="telefon" value="">
+<br><br>
+e-posta adresi:<INPUT TYPE="TEXT" name="eposta" value="">
+<br><br>
+Adresi:<INPUT TYPE="TEXT" name="adres" value="">
+<br><br>
+<INPUT TYPE="SUBMIT" name="gonder" value="Kaydet"> </FORM>
+</body>
+</html>
