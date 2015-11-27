bb529fe42cdc9e8c30d3ba0dc7c041ea90935952
Gurali <rutz.zari@gmail.com>
2015-11-27 20:17:31 +0200

    Update kaydet.php

--- kaydet.php
+++ kaydet.php
@@ -1,4 +1,14 @@
+<?php
+
+ob_start();
+session_start();
+if($_SESSION['giris'] != 1){
+	header('location:giris.php');
+	exit;
+}
+?>
 <html>
+<title>Rehber</title>
 <body>
 <FORM action="kaydett.php" method="post">
 Adı:<INPUT TYPE="TEXT" name="ad" value="">
@@ -12,5 +22,6 @@ e-posta adresi:<INPUT TYPE="TEXT" name="eposta" value="">
 Adresi:<INPUT TYPE="TEXT" name="adres" value="">
 <br><br>
 <INPUT TYPE="SUBMIT" name="gonder" value="Kaydet"> </FORM>
+<button onclick="window.location.href='yeni.php'">Ana Sayfa</button>
 </body>
 </html>
