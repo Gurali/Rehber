d12ec3b4e5f71248f219f1e9d933ab21c4956edd
Gurali <rutz.zari@gmail.com>
2015-11-27 20:18:00 +0200

    Update kaydett.php

--- kaydett.php
+++ kaydett.php
@@ -1,19 +1,33 @@
 <?php 
+ob_start();
+session_start();
+if($_SESSION['giris'] != 1){
+	header('location:giris.php');
+	exit;
+}
 include "farkli.php";
+if($_POST){
+$no = $_POST["no"];
 $ad = $_POST["ad"];
 $soyad=$_POST["soyad"];
 $telefon=$_POST["telefon"];
 $eposta=$_POST["eposta"];
 $adres=$_POST["adres"];
+}
+else{
+	header('location:kaydet.php');
+	exit;
+}
 try{
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$kayit="INSERT INTO tbl_kisiler (ad,soyad,telefon,eposta,adres) VALUES($ad,$soyad,$telefon,$eposta,$adres)";
-$dbh->exec($kayit);
-echo"kayıt yapildi.";
-	
+	$kayit = "INSERT INTO tbl_kisiler (ad,soyad,telefon,eposta,adres) VALUES('$ad','$soyad','$telefon','$eposta','$adres')";
+	$dbh->query($kayit);
+    echo 'Kayıt Eklendi.';
 	}
-    catch{
-		echo "Kayıt yapılamadı!";
-	}
-
+    catch (PDOException $e) { 
+    throw new pdoDbException($e); 
+} 
 ?>
+<html>
+<title>Rehber</title>
+<meta http-equiv="refresh" content="1;URL=yeni.php">
+</html>
